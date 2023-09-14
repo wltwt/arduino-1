fbc9bdc95d3eba75fe1ca10c97b0c2c28e181ea5
wltwt <35377994+wltwt@users.noreply.github.com>
2023-09-14 13:50:32 +0200

    added ultrasonic sensor

--- led-strip/led-strip.ino
+++ led-strip/led-strip.ino
@@ -2,6 +2,8 @@
 
 const int btnPin = 4;
 const int ledPin = 3;
+const int trigPin = 8;
+const int echoPin = 9;
 const int lightSensorPin = A1;
 int reading;
 int btnState;
@@ -16,11 +18,14 @@ int lastButtonState = LOW;  // the previous reading from the input pin
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;  
+long duration, cm;
 
 void setup(){
     pinMode(ledPin, OUTPUT);
     pinMode(btnPin, INPUT);
     pinMode(lightSensorPin, INPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
     digitalWrite(ledPin, ledState);
     Serial.begin(9600);
 }
@@ -44,6 +49,16 @@ void loop(){
         // }
     // }
 
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+    cm = (duration / 2) /29.1;
+
     if (reading != lastButtonState) {
     // reset the debouncing timer
         lastDebounceTime = millis();
@@ -57,7 +72,10 @@ void loop(){
                 ledState = !ledState;
                 result = addTwoInts(3, buttonState);
             }
-            Serial.println(result);
+            Serial.print("Distance: ");
+            Serial.print(cm);
+            Serial.print(" cm \n");
+            //Serial.println(result);
         }
     }
 
